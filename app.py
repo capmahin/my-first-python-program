881570bf7ac0e95ce66bac3f275258c76296cb3c
capmahin <mahinulhuque1996@gmail.com>
2025-07-18 00:35:10 +0600

    add

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("Hello World")
+students_count = 1000
+rating = 4.99
+is_published = True
+course_name = "Python Programming"
+print(students_count)
+print(rating)
+print(is_published)
+print(course_name)
