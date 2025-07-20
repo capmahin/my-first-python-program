9732bec4bb42b760ff566b9b65a4f8adc9b7e5b5
capmahin <mahinulhuque1996@gmail.com>
2025-07-21 04:33:55 +0600

    add

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-students_count = 1000
-rating = 4.99
-is_published = True
-course_name = "Python Programming"
-print(students_count)
-print(rating)
-print(is_published)
-print(course_name)
+course = "Python Programming"
+print(len(course))
+print(course[0])
+print(course[-1])
+print(course[0:3])
+print(course[0:])
+print(course[:3])
+print(course[:])
