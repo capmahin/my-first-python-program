e68565de5dfb29157c33d22de1d899586d0f2045
capmahin <mahinulhuque1996@gmail.com>
2025-07-22 02:51:40 +0600

    add strings

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-first = "Mosh"
-last = "Hamedani"
-full = f"{len(first)} {2+2}"
-print(full)
+course = "  python programming"
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.rstrip())
+print(course.find("pro"))
+print(course.replace("p", "j"))
+print("pro" in course)
+print("swift" not in course)
