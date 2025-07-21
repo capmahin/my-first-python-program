470ef2ffe504fa71077c73f3118211133970f530
capmahin <mahinulhuque1996@gmail.com>
2025-07-21 20:50:54 +0600

    add

--- app.py
+++ app.py
@@ -1,8 +1,2 @@
-course = "Python Programming"
-print(len(course))
-print(course[0])
-print(course[-1])
-print(course[0:3])
-print(course[0:])
-print(course[:3])
-print(course[:])
+course = "Python \"Programming"
+print(course)
