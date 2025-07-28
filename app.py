f357f6bc13318f80e21717b63092819a71aeee69
capmahin <mahinulhuque1996@gmail.com>
2025-07-28 21:33:49 +0600

    relpace and to or

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-high_income = True
+high_income = False
 good_credit = True
 
-if high_income and good_credit:
+if high_income or good_credit:
     print("Eligible")
+
+
+else:
+    print("Not eligible")
